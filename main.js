ef082283943ce4db752743455ff03b6abc5f1d0b
Taksh Patel <88882602+TakshPatel26@users.noreply.github.com>
2021-12-16 18:22:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+webcam.attach('#camera');
+camera=document.getElementById("camera");
+Webcam.set({
+     width:350,
+     height:300,
+     image_format:'png',
+     png_quality:90
+});
+function take_snapshot(){
+Webcam.snap(function(data_uri){
+     document.getElementById("result").innerHTML='<img id="selfie_image" src=" '+data_uri+' "/>';
+});
+}
